54eb63cf30f7f208e8344a020c266a80bc934cf3
hwangjungmin <hwangstar156@gmail.com>
2024-05-13 23:56:42 +0900

    feat: 빌드타임에 미리 생성해놓은 정적 url을 제외한 url은 404가 나올 수 있도록 처리

--- app/wiki/[topic]/page.tsx
+++ app/wiki/[topic]/page.tsx
@@ -1,4 +1,19 @@
-export default function Topic({ params: { topic }, searchParams }) {
+import { ReadonlyURLSearchParams } from 'next/navigation'
+
+export const dynamicParams = false
+
+export function generateStaticParams() {
+  return [{ topic: 'Next.js' }, { topic: 'Typescript' }, { topic: 'react-query' }]
+}
+
+interface TopicProps {
+  params: {
+    topic: string
+  }
+  searchParams: ReadonlyURLSearchParams
+}
+
+export default function Topic({ params: { topic }, searchParams }: TopicProps) {
   console.log({ topic, searchParams })
 
   return <div>{topic}</div>
