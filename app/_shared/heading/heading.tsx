97789b76972d01a2075bc074340a99f9c979734c
hwangjungmin <hwangstar156@gmail.com>
2024-05-17 01:59:24 +0900

    feat: heading component 추가

---
+++ app/_shared/heading/heading.tsx
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components'
+
+const Heading = css`
+  font-weight: 700;
+`
+
+export const H1 = styled.h1`
+  ${Heading}
+  font-size: 48px;
+`
+export const H2 = styled.h2`
+  ${Heading}
+  font-size: 24px;
+`
+export const H3 = styled.h3`
+  ${Heading}
+  font-size: 22px;
+`
+export const H4 = styled.h4`
+  ${Heading}
+  font-size: 20px;
+`
+export const H5 = styled.h5`
+  ${Heading}
+  font-size: 20px;
+`
+export const H6 = styled.h6`
+  ${Heading}
+  font-size: 20px;
+`
