92693893fb887fb47a0bdb5f81f160ec07991cc5
hwangjungmin <hwangstar156@gmail.com>
2024-05-18 04:01:06 +0900

    feat: 기본 베이스 페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
 import { LinkToTest } from './_components/link-to-test'
-import { Accordion, AccordionDescription, AccordionTitle } from './_shared/accordion'
-import { H2 } from './_shared/heading'
 
 export default function Home() {
-  return (
-    <Accordion>
-      <AccordionTitle>
-        <H2>1. 개요</H2>
-      </AccordionTitle>
-      <AccordionDescription>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam illum similique expedita
-        tempore rerum, doloremque aut ducimus dolor asperiores eum fugit, ad voluptatum quam beatae
-        iste reiciendis officia, aspernatur enim.
-      </AccordionDescription>
-    </Accordion>
-  )
+  return <LinkToTest />
 }
