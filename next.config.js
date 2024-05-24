f4daf40d7a4f174167893e2a1a5b104796283560
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-25 06:19:54 +0900

    chore: next.config mdx 모듈 추가

--- next.config.js
+++ next.config.js
@@ -1,10 +1,43 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+import remarkGfm from 'remark-gfm'
+import mdx from '@next/mdx'
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {
+    providerImportSource: '@mdx-js/react',
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
   compiler: {
     styledComponents: true,
   },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  async redirects() {
+    return [
+      // www to non-www redirect
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.dewiki.vercel.app' }],
+        destination: 'https://dewiki.vercel.app/:path*',
+        permanent: true,
+        statusCode: 301,
+      },
+      {
+        source: '/wiki',
+        destination: '/',
+        permanent: true,
+        statusCode: 301,
+      },
+    ]
+  },
 }
+
+export default withMDX(nextConfig)
