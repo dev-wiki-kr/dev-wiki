8d7efd9bb95e13dcc0dfefd37634406d3776114e
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-25 06:22:45 +0900

    feat: post 페이지 구현

---
+++ app/post/[topic]/components/container.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import styled from 'styled-components'
+
+export function Container({ children }: { children: React.ReactNode }) {
+  return <StyledContainer>{children}</StyledContainer>
+}
+
+const StyledContainer = styled.article`
+  width: 768px;
+  margin: 100px auto;
+`
