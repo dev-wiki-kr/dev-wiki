294d93a60039d74c1068970deeb23d3bc906768c
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-05 19:44:55 +0900

    chore: next/image 제거

--- app/post/[topic]/components/mdx-components.tsx
+++ app/post/[topic]/components/mdx-components.tsx
@@ -7,11 +7,9 @@ import {
 } from '../../../_shared/accordion'
 import { H1, H2, H3 } from '../../../_shared/heading'
 import NextLink from './next-link'
-import NextImage from './next-image'
 
 export const mdxComponents: MDXComponents = {
   a: NextLink,
-  img: NextImage,
   h1: ({ children }) => <H1>{children}</H1>,
   h2: ({ children }) => <H2>{children}</H2>,
   h3: ({ children }) => <H3>{children}</H3>,
