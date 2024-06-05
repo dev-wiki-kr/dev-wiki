8cf86191bbfd1c52a5a3fbd80a87d52b7ef35f5c
chaduhwan <enghks2973@naver.com>
2024-06-05 17:16:04 +0900

    chore : 다이나믹 서브셋 적용

--- app/_styles/global-style.tsx
+++ app/_styles/global-style.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { createGlobalStyle } from 'styled-components'
-import { media } from './media'
 
 export const GlobalStyle = createGlobalStyle`
     *,
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 import Script from 'next/script'
 
 import localFont from 'next/font/local'
+import { GlobalFont } from '../public/font/global-font'
+// import localFont from 'next/font/local'
 interface LayoutProps {
   children: React.ReactNode
 }
@@ -23,17 +25,17 @@ export const metadata: Metadata = {
     canonical: 'https://dewiki.vercel.app',
   },
 }
-const pretendard = localFont({
-  src: '../public/font/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
-  variable: '--font-pretendard',
-})
+// const pretendard = localFont({
+//   src: '../public/font/PretendardVariable.woff2',
+//   display: 'swap',
+//   weight: '45 920',
+//   variable: '--font-pretendard',
+// })
 //QUESTION: 위키피디아 형식에서는 어떤 json-ld 구조화형식이 맞을지..? 아니면 안해도 될지
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="kr" className={pretendard.className}>
+    <html>
       <head>
         <GoogleAnalytics gaId="G-MTBJ5VNE9S" />
         <Script id="clarity-script" strategy="afterInteractive">
@@ -48,6 +50,7 @@ export default function RootLayout({ children }: LayoutProps) {
       </head>
       <body>
         <StyledComponentsRegistry>
+          <GlobalFont />
           <GlobalStyle />
           <Header />
           {children}
