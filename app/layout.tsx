e17792a96024647889d8cad43781a9034b073945
Zalgritte <Zalgritte@gmail.com>
2024-06-07 16:19:52 +0900

    MS Clarity 연동
    Layout.tsx에 Script 임포트
    <head>에 clarity 코드 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { StyledComponentsRegistry } from './_external/styled-components'
 import { GlobalStyle } from './_styles/global-style'
 import { Metadata } from 'next'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -29,6 +30,15 @@ export default function RootLayout({ children }: LayoutProps) {
     <html>
       <head>
         <GoogleAnalytics gaId="G-MTBJ5VNE9S" />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "mo3ghx9sv0");
+          `}
+        </Script>
       </head>
       <body>
         <StyledComponentsRegistry>
