295a299d33ce4c00274d6ab46d772d9d69208155
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-09 17:37:46 +0900

    refactor: mdx components props 전달 방식 변경

--- app/post/[topic]/components/mdx-components.tsx
+++ app/post/[topic]/components/mdx-components.tsx
@@ -5,14 +5,17 @@ import {
   AccordionProvider,
   AccordionTitle,
 } from '../../../_shared/accordion'
-import { H1, H2, H3 } from '../../../_shared/heading'
+import { H1, H2, H3, H4, H5, H6 } from '../../../_shared/heading'
 import { NextLink } from './next-link'
 
 export const mdxComponents: MDXComponents = {
   a: NextLink,
-  h1: ({ children }) => <H1>{children}</H1>,
-  h2: ({ children }) => <H2>{children}</H2>,
-  h3: ({ children }) => <H3>{children}</H3>,
+  h1: (props) => <H1 {...props} />,
+  h2: (props) => <H2 {...props} />,
+  h3: (props) => <H3 {...props} />,
+  h4: (props) => <H4 {...props} />,
+  h5: (props) => <H5 {...props} />,
+  h6: (props) => <H6 {...props} />,
   Accordion: (props) => <Accordion {...props} />,
   AccordionProvider: (props) => <AccordionProvider {...props} />,
   AccordionTitle: (props) => <AccordionTitle {...props} />,
