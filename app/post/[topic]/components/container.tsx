9a5d697fc1966ec7775c41936d7f76094b18bea6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-09 17:37:00 +0900

    feat: post layout 마진 추가

--- app/post/[topic]/components/container.tsx
+++ app/post/[topic]/components/container.tsx
@@ -6,7 +6,9 @@ export function Container({ children }: { children: React.ReactNode }) {
   return <StyledContainer>{children}</StyledContainer>
 }
 
-const StyledContainer = styled.article`
-  width: 768px;
-  margin: 100px auto;
+const StyledContainer = styled.div`
+  max-width: 768px;
+  margin-top: 5.5rem;
+  margin-left: auto;
+  margin-right: auto;
 `
