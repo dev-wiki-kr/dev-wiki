c82ed617c3f4f26ecefc569e4acb1647e30c282f
chaduhwan <enghks2973@naver.com>
2024-06-10 21:56:43 +0900

    feat/ 검색 UI 구현

--- app/_components/search/search.tsx
+++ app/_components/search/search.tsx
@@ -1,18 +1,42 @@
 import { styled } from 'styled-components'
 import { media } from '../../_styles/media'
 
+const SearchCon = styled.div`
+  width: 420px;
+  height: 40px;
+  border: #dddddd solid;
+  border-radius: 8px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  &:hover,
+  &:focus-within,
+  &:active {
+    border: #b5b5b5 solid;
+  }
+`
+const Input = styled.input`
+  width: 356px;
+  height: 24px;
+  margin: 8px 0px;
+  margin-right: 16px;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`
+
 const Icon = styled.img`
-  ${media.phone`
-    height: 250px;
-  `}
+  margin: 8px 12px;
+  font-size: 16px;
 `
 
 export function Search() {
   return (
-    <form>
+    <SearchCon>
       <Icon src="images/search-icon.svg" />
-      <input />
-      <button />
-    </form>
+      <Input placeholder="검색어를 입력해 주세요." />
+      <div></div>
+    </SearchCon>
   )
 }
