bc1e10668833630d486c42f1a9f55c84fe231880
chaduhwan <enghks2973@naver.com>
2024-06-10 20:53:32 +0900

    feat/UI Icon svg 파일 추가

--- app/_components/link-to-test.tsx
+++ app/_components/link-to-test.tsx
@@ -2,11 +2,17 @@
 
 import Link from 'next/link'
 import { styled } from 'styled-components'
+import { Search } from './search/search'
 
 const StyledLink = styled(Link)`
   font-size: 24px;
 `
 
 export function LinkToTest() {
-  return <StyledLink href={'/wiki/Next.js'}>Next.js 위키로 이동하기</StyledLink>
+  return (
+    <div>
+      <StyledLink href={'/wiki/Next.js'}>Next.js 위키로 이동하기</StyledLink>
+      <Search />
+    </div>
+  )
 }
