17a42417a7106366cc908834b96b403901f9d9da
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-11 21:04:03 +0900

    feat: toc top 추가

---
+++ app/_shared/toc/toc-top.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+import type { Toc } from '../../lib/get-toc'
+import Link from 'next/link'
+import { media } from '../../_styles/media'
+
+interface TocSideProps {
+  tableOfContents: Toc[]
+}
+
+const TocWrapper = styled.div`
+  display: none; // 기본적으로 숨김
+
+  ${media.laptop`
+    display: block;
+     margin-bottom: 20px;
+  `}
+`
+
+const TocList = styled.ul`
+  all: unset;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
+const TocItem = styled.li.withConfig({
+  shouldForwardProp: (prop) => prop !== 'level',
+})<{ level: number }>`
+  font-size: 14px;
+  font-weight: 500;
+  color: #777777;
+  transform: scale(1);
+  padding-left: ${({ level }) => (level > 1 ? `${(level - 1) * 8}px` : '0')};
+
+  &:hover {
+    font-weight: 600;
+    color: #222222;
+  }
+
+  transition: all 0.125s ease-in-out 0s;
+`
+
+const Text = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  color: #222222;
+`
+
+export const TocTop = ({ tableOfContents }: TocSideProps) => {
+  if (tableOfContents.length === 0) {
+    return null
+  }
+
+  return (
+    <TocWrapper>
+      <TocList role="list">
+        <Text>목차</Text>
+        {tableOfContents.map((toc, i) => (
+          <TocItem key={i} level={toc.level} data-level={toc.level}>
+            <Link href={`#${toc.slug}`}>{toc.text}</Link>
+          </TocItem>
+        ))}
+      </TocList>
+    </TocWrapper>
+  )
+}
