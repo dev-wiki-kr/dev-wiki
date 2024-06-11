33904fd0b6e9c47612b18d94bab99904b9ac1fcb
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-11 21:02:30 +0900

    refactor: getPostBySlug 함수 구조 변경

---
+++ app/lib/posts.ts
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+export interface PostMatter {
+  title: string
+  date: Date
+  dateString: string
+  thumbnail: string
+  description: string
+  lastModified: string
+}
+
+export interface Post extends PostMatter {
+  url: string
+  slug: string
+  content: string
+}
+
+const BASE_PATH = path.join(process.cwd(), 'posts')
+
+// MDX 상세 가져오기
+const parsePostDetail = async (postPath: string) => {
+  const file = fs.readFileSync(postPath, 'utf8')
+  const { data, content } = matter(file)
+  const grayMatter = data as PostMatter
+  const lastModified = grayMatter.lastModified
+  return { ...grayMatter, content, lastModified }
+}
+
+// 슬러그 가져오기
+export const getPostSlugs = () => {
+  return fs.readdirSync(BASE_PATH).filter((file) => path.extname(file) === '.mdx')
+}
+
+// 상세 내용 가져오기
+export const getPostBySlug = async (slug: string) => {
+  const filePath = path.join(BASE_PATH, `${slug}.mdx`)
+  return await parsePostDetail(filePath)
+}
