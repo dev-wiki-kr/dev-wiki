7315cd8e3f901b51c44527424fe4cad1eed502a6
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-15 20:27:56 +0900

    feat: bottom sheet body 추가

---
+++ app/_shared/bottom-sheet/bottom-sheet-body.tsx
@@ -0,0 +1,16 @@
+import { HTMLProps, forwardRef } from 'react'
+import styled from 'styled-components'
+
+const BodyWrapper = styled.div`
+  padding: 0.5rem 0.75rem;
+`
+
+export const BottomSheetBody = forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(
+  function BottomSheetBody(props, ref) {
+    return (
+      <BodyWrapper ref={ref} {...props}>
+        {props.children}
+      </BodyWrapper>
+    )
+  },
+)
