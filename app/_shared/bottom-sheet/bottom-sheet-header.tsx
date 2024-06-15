0293e19560e20ebcece3dd8736d69fdd1ba9b9dd
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-15 20:26:56 +0900

    feat: bottom sheet header 추가

---
+++ app/_shared/bottom-sheet/bottom-sheet-header.tsx
@@ -0,0 +1,19 @@
+import React, { HTMLProps, forwardRef } from 'react'
+import { styled } from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0.5rem 1rem 0.5rem 0.75rem;
+`
+
+export const BottomSheetHeader = forwardRef<HTMLHeadingElement, HTMLProps<HTMLHeadingElement>>(
+  function BottomSheetHeader(props, ref) {
+    return (
+      <HeaderWrapper ref={ref} {...props}>
+        {props.children}
+      </HeaderWrapper>
+    )
+  },
+)
