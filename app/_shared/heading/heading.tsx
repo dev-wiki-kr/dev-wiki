5ef1b14264abe2a67ed1f4ebee39b4f5794ae03e
hwangstar156 <hwangstar156@gmail.com>
2024-06-21 00:28:09 +0900

    custom heading 태그 추가

--- app/_shared/heading/heading.tsx
+++ app/_shared/heading/heading.tsx
@@ -65,3 +65,27 @@ export const H6 = styled.h6`
    font-size: 18px;
   `}
 `
+
+interface CustomHeadingProps {
+  level: number
+  children: React.ReactNode
+}
+
+export function CustomHeading({ level, children }: CustomHeadingProps) {
+  switch (level) {
+    case 1:
+      return <H1>{children}</H1>
+    case 2:
+      return <H2>{children}</H2>
+    case 3:
+      return <H3>{children}</H3>
+    case 4:
+      return <H4>{children}</H4>
+    case 5:
+      return <H5>{children}</H5>
+    case 6:
+      return <H6>{children}</H6>
+    default:
+      return null
+  }
+}
