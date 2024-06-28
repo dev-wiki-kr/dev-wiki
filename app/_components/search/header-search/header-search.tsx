05cfcd1370a71899949131a7d2a1bb399f38c4f5
hwangstar156 <hwangstar156@gmail.com>
2024-06-28 21:20:17 +0900

    feat: header-search를 클릭할때 main-search가 열릴 수 있도록 수정

--- app/_components/search/header-search/header-search.tsx
+++ app/_components/search/header-search/header-search.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import { styled } from 'styled-components'
-import { useHeaderSearch } from '../hook/use-header-search'
-import { SearchResultPopover } from '../search-result-popover/search-result-popover'
+import { useSearchContainerPosition } from '../context'
 
 const Container = styled.div`
   position: relative;
 `
 
-const SearchCon = styled.div<{ $isOpen: boolean }>`
+const SearchContainer = styled.div`
   width: 240px;
   height: 40px;
   border: #dddddd solid;
@@ -18,17 +17,6 @@ const SearchCon = styled.div<{ $isOpen: boolean }>`
   z-index: 1;
   align-items: center;
   display: flex;
-
-  &:hover {
-    border: #b5b5b5 solid;
-  }
-  ${(props) =>
-    props.$isOpen &&
-    `
-  border: #b5b5b5 solid;
-    box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.25);
-    border-radius: 8px 8px 0px 0px;
-  `}
 `
 
 const Input = styled.input`
@@ -48,44 +36,21 @@ const SearchIcon = styled.img`
   height: 24px;
   margin: 8px 12px;
 `
-const XIcon = styled.img`
-  width: 24px;
-  height: 24px;
-  margin-right: 8px;
-  cursor: pointer;
-  position: relative;
-  z-index: 1;
-`
 
 export function HeaderSearch() {
-  const { handleEnterKeydown, handleModal, inputRef, isOpen, keyword, searchConRef, setKeyword } =
-    useHeaderSearch()
+  const { searchNode } = useSearchContainerPosition()
+
+  const handleClickSearch = () => {
+    searchNode?.click()
+    searchNode?.querySelector('input')?.focus()
+  }
 
   return (
     <Container>
-      <SearchCon ref={searchConRef} onClick={handleModal} $isOpen={isOpen}>
+      <SearchContainer onClick={handleClickSearch}>
         <SearchIcon src="images/search-icon.svg" />
-        <Input
-          value={keyword}
-          onChange={(e) => {
-            setKeyword(e.target.value)
-          }}
-          onKeyDown={handleEnterKeydown}
-          placeholder="검색어를 입력해 주세요."
-          ref={inputRef}
-        />
-        {keyword && (
-          <XIcon
-            src="images/X-mark.svg"
-            onClick={(e) => {
-              e.stopPropagation()
-              setKeyword('')
-              if (inputRef.current) inputRef.current.focus()
-            }}
-          />
-        )}
-      </SearchCon>
-      <SearchResultPopover isOpen={isOpen} handleModal={handleModal} keyword={keyword} />
+        <Input placeholder="검색어를 입력해 주세요." />
+      </SearchContainer>
     </Container>
   )
 }
