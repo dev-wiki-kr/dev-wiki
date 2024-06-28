98cfc7fdc1bb99dfcca2295229f7dc45ff0441b8
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-29 06:36:15 +0900

    feat: post container 마진 추가

--- app/post/[topic]/components/container.tsx
+++ app/post/[topic]/components/container.tsx
@@ -9,9 +9,10 @@ export function Container({ children }: { children: React.ReactNode }) {
 
 const StyledContainer = styled.article`
   max-width: 768px;
-  margin: 5.5rem auto 0 auto;
+  margin: 4rem auto 0 auto;
 
   ${media.phone`
+    margin: 2.5rem auto 0 auto;
     padding: 0 12px;
   `}
 `
