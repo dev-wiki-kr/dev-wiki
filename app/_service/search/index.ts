b494f10b01a2128e4100c819cf20bbdb8a57d8b8
hwangstar156 <hwangstar156@gmail.com>
2024-06-29 05:01:49 +0900

    feat: 수정된 검색 api에 맞춰 response 수정

--- app/_service/search/index.ts
+++ app/_service/search/index.ts
@@ -1,116 +1,10 @@
-export interface SearchAutocompleteResponse {
-  body: Body
-  statusCode: number
-  headers: Headers
-  meta: Meta
-}
-
-export interface Body {
-  took: number
-  timed_out: boolean
-  _shards: Shards
-  hits: Hits
-  suggest: Suggest
-}
-
-export interface Shards {
-  total: number
-  successful: number
-  skipped: number
-  failed: number
-}
-
-export interface Hits {
-  total: Total
-  max_score: any
-  hits: any[]
-}
-
-export interface Total {
-  value: number
-  relation: string
-}
-
-export interface Suggest {
-  autocomplete: Autocomplete[]
-}
-
-export interface Autocomplete {
-  text: string
-  offset: number
-  length: number
-  options: Option[]
-}
-
-export interface Option {
-  text: string
-  _index: string
-  _id: string
-  _score: number
-  _source: Source
-}
-
-export interface Source {
+interface SearchAutocompleteResponse {
+  id: number
+  shortTitle: string
   title: string
   content: string
-}
-
-export interface Headers {
-  date: string
-  'content-type': string
-  'content-length': string
-  connection: string
-  'access-control-allow-origin': string
-}
-
-export interface Meta {
-  context: any
-  request: Request
-  name: string
-  connection: Connection
-  attempts: number
-  aborted: boolean
-}
-
-export interface Request {
-  params: Params
-  options: Options
-  id: number
-}
-
-export interface Params {
-  method: string
-  path: string
-  body: string
-  querystring: string
-  headers: Headers2
-  timeout: number
-}
-
-export interface Headers2 {
-  'user-agent': string
-  'content-type': string
-  'content-length': string
-}
-
-export interface Options {}
-
-export interface Connection {
-  url: string
-  id: string
-  headers: Headers3
-  deadCount: number
-  resurrectTimeout: number
-  _openRequests: number
-  status: string
-  roles: Roles
-}
-
-export interface Headers3 {}
-
-export interface Roles {
-  data: boolean
-  ingest: boolean
+  createdAt: string
+  updatedAt: string
 }
 
 export async function getSearchAutocomplete(keyword: string) {
@@ -120,9 +14,9 @@ export async function getSearchAutocomplete(keyword: string) {
     }
     const res = await fetch(`https://devwiki.co.kr/wiki-api/search/autocomplete?q=${keyword}`)
 
-    const json = (await res.json()) as SearchAutocompleteResponse
+    const data = (await res.json()) as SearchAutocompleteResponse[]
 
-    return json.body.suggest.autocomplete[0].options
+    return data
   } catch (error) {
     console.log(error)
     return null
