0d2ae2d64ffc8129846a93f6e7d6ca1993b0bde2
hwangstar156 <hwangstar156@gmail.com>
2024-06-28 22:03:41 +0900

    feat: user-agent를 통한 isMobile 함수 추가

---
+++ app/_util/platform.ts
@@ -0,0 +1,11 @@
+export function isMobileServer(userAgent: string) {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone|IEMobile|Opera Mini/i.test(
+    userAgent,
+  )
+}
+
+export function isMobileClient() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone|IEMobile|Opera Mini/i.test(
+    navigator.userAgent,
+  )
+}
