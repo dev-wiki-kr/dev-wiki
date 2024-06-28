0474174c0444c7a2a9a7b852fd043326a7e7de5e
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-29 06:35:53 +0900

    feat: post header 마크업

--- app/post/[topic]/components/post-header.tsx
+++ app/post/[topic]/components/post-header.tsx
@@ -1,5 +1,36 @@
-import { H1 } from '../../../_shared/heading'
+'use client'
 
-export const PostHeader = ({ title }: { title: string }) => {
-  return <H1>{title}</H1>
+import styled from 'styled-components'
+import { H1 } from '../../../_shared/heading'
+import { formatDate } from '../../../_util/format'
+
+const HeaderWrapper = styled.div`
+  margin-bottom: 40px;
+`
+
+const TitleWrapper = styled.div`
+  margin-block-start: 20px;
+  margin-block-end: 24px;
+`
+
+const Text = styled.p`
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 1.4;
+  color: #777777;
+
+  margin: 0;
+`
+
+export const PostHeader = ({ title, date }: { title: string; date: Date }) => {
+  const updatedAt = formatDate(date.toString())
+  return (
+    <HeaderWrapper>
+      <Text>최근 수정 시각</Text>
+      <Text>{updatedAt}</Text>
+      <TitleWrapper>
+        <H1>{title}</H1>
+      </TitleWrapper>
+    </HeaderWrapper>
+  )
 }
