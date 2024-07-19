24ec2ecbc4f295f5ac016d09b042f1e583224ad2
chaduhwan <enghks2973@naver.com>
2024-07-19 09:50:16 +0900

    feat/modal 상태에 따라 hash route 추가

--- app/_shared/modal/useModal.tsx
+++ app/_shared/modal/useModal.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter, usePathname } from 'next/navigation'
 
 export function useModal() {
   const [isOpen, setIsOpen] = useState(false)
+  const router = useRouter()
+  const pathname = usePathname()
+
   const handleModal = () => {
     setIsOpen((prevState) => !prevState)
   }
+
+  useEffect(() => {
+    if (isOpen) {
+      router.push(`${pathname}#modal`, { scroll: false })
+    } else {
+      router.push(`${pathname}`)
+    }
+  }, [isOpen])
   return { isOpen, handleModal }
 }
