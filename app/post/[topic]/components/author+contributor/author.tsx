261f8c6a7c1a1bc68f71b7df6b288bb3ff125023
chaduhwan <enghks2973@naver.com>
2024-08-16 22:27:52 +0900

    feat: contributors 더보기 기능 추가

--- app/post/[topic]/components/author+contributor/author.tsx
+++ app/post/[topic]/components/author+contributor/author.tsx
@@ -2,10 +2,11 @@
 
 import styled from 'styled-components'
 import { AuthorInfo } from './author-info'
+import { ContributorInfo } from './contributor-info'
+import { useState } from 'react'
 
 const Container = styled.div`
   width: 1024px;
-  height: 250px;
   padding-top: 20px;
 `
 
@@ -16,12 +17,16 @@ const InfoMessage = styled.p`
 
 const AuthorContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  width: 100%;
 `
 
 const ShowMore = styled.div`
+  display: flex;
   color: #007df0;
   font-size: 18px;
   cursor: pointer;
+  justify-content: center;
 `
 
 interface AuthorInterface {
@@ -33,15 +38,41 @@ interface ContributorInfoInterface {
   username: string
 }
 
-export function Author({ authorData }: AuthorInterface) {
+export function Author({ authorData, contributorData }: AuthorInterface) {
+  const showAll = contributorData.length > 6
+  const displayedContributors = showAll ? contributorData.slice(0, 5) : contributorData
+  const showAllContrubutors = showAll ? contributorData.slice(5) : null
+
+  const [isOpen, setOpen] = useState(false)
+  function handleOpen() {
+    setOpen(!isOpen)
+  }
   return (
     <Container>
       <hr />
       <InfoMessage>작성자 및 기여자</InfoMessage>
       <AuthorContainer>
         <AuthorInfo src={authorData!.avartarUrl!} username={authorData.username!} />
+        {displayedContributors.map((contributor, index) => (
+          <ContributorInfo
+            src={contributor.avartarUrl}
+            username={contributor.username}
+            key={index}
+          />
+        ))}
       </AuthorContainer>
-      <ShowMore>더 보기 ↓</ShowMore>
+      {isOpen && (
+        <AuthorContainer>
+          {showAllContrubutors?.map((contributor, index) => (
+            <ContributorInfo
+              src={contributor.avartarUrl}
+              username={contributor.username}
+              key={index}
+            />
+          ))}
+        </AuthorContainer>
+      )}
+      {showAll && <ShowMore onClick={handleOpen}>더 보기 ↓</ShowMore>}
     </Container>
   )
 }
